0c39b8e6d1f5a0f8dcd7248f28cc307272288c91
gougonst <gougonst@gmail.com>
2025-05-15 23:47:19 +0800

    [FIX] 797 - All Paths From Source to Target - Delete unnecessary variable

--- include/all_paths_from_source_to_target.h
+++ include/all_paths_from_source_to_target.h
@@ -5,5 +5,5 @@ class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph);
 private:
-    void _dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, vector<int>& path, unordered_set<int>& visited, int cur, int target);
+    void _dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, vector<int>& path, int cur, int target);
 };
--- src/all_paths_from_source_to_target.cpp
+++ src/all_paths_from_source_to_target.cpp
@@ -1,16 +1,14 @@
 #include "all_paths_from_source_to_target.h"
-#include <unordered_set>
 
 vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
     int target = graph.size() - 1;
     vector<vector<int>> allPaths;
     vector<int> path{0};
-    unordered_set<int> visited;
     _dfs(graph, allPaths, path, visited, 0, target);
     return allPaths;
 }
 
-void Solution::_dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, vector<int>& path, unordered_set<int>& visited, int cur, int target) {
+void Solution::_dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, vector<int>& path, int cur, int target) {
     if (cur == target) {
         allPaths.push_back(path);
         return;
@@ -18,7 +16,7 @@ void Solution::_dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, v
 
     for (auto& neighbor: graph[cur]) {
         path.push_back(neighbor);
-        _dfs(graph, allPaths, path, visited, neighbor, target);
+        _dfs(graph, allPaths, path, neighbor, target);
         path.pop_back();
     }
 }
