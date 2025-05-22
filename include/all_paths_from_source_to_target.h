0f3675a118f7fdfef98691648437799f1bbeb9ee
gougonst <gougonst@gmail.com>
2025-05-22 23:20:03 +0800

    [MOD] 797. All Paths From Source to Target - BFS

--- include/all_paths_from_source_to_target.h
+++ include/all_paths_from_source_to_target.h
@@ -3,7 +3,8 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph);
+    vector<vector<int>> allPathsSourceTarget1(vector<vector<int>>& graph);
+    vector<vector<int>> allPathsSourceTarget2(vector<vector<int>>& graph);
 private:
     void _dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, vector<int>& path, int cur, int target);
 };
--- src/all_paths_from_source_to_target.cpp
+++ src/all_paths_from_source_to_target.cpp
@@ -1,6 +1,7 @@
 #include "all_paths_from_source_to_target.h"
+#include <queue>
 
-vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+vector<vector<int>> allPathsSourceTarget1(vector<vector<int>>& graph) {
     int target = graph.size() - 1;
     vector<vector<int>> allPaths;
     vector<int> path{0};
@@ -19,4 +20,26 @@ void Solution::_dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, v
         _dfs(graph, allPaths, path, neighbor, target);
         path.pop_back();
     }
+}
+
+vector<vector<int>> allPathsSourceTarget2(vector<vector<int>>& graph) {
+    int target = graph.size() - 1;
+    vector<vector<int>> allPaths;
+    queue<vector<int>> q;
+    q.push({0});
+    while (!q.empty()) {
+        vector<int> candidate = q.front();
+        q.pop();
+        int last = candidate[candidate.size() - 1];
+        if (last == target) {
+            allPaths.push_back(candidate);
+            continue;
+        }
+        for (auto& neighbor: graph[last]) {
+            vector<int> newCandidate(candidate);
+            newCandidate.push_back(neighbor);
+            q.push(newCandidate);
+        }
+    }
+    return allPaths;
 }
